a9661ae94aebee4df9266979b0c6476db7bc98a3
njuptjsy <1192937986@qq.com>
2015-05-09 01:33:10 +0800

    1.delete unused folder

--- AgileJavaNote-master/AgileJavaNote-master/AgileJavaNote-master/src/sis/report/ReportConstant.java
+++
@@ -1,8 +0,0 @@
-﻿package sis.report;
-
-public class ReportConstant {
-	/*
-	 * 这种仅仅持有常用类常量的真空类具有争议，应该谨慎使用
-	 * */
-	public static final String NEWLINE = System.getProperty("line.separator");
-}
--- AgileJavaNote-master/AgileJavaNote-master/AgileJavaNote-master/src/sis/studentinfo/GradingStrategy.java
+++
@@ -1,5 +0,0 @@
-package sis.studentinfo;
-
-public interface GradingStrategy {//接口中所有的方法都需要在实现类中进行实现，所有接口所有的方法默认都是public和abstract，不需要声明
-	int getGradePointsFor(Student.Grade grade);
-}
